8a3b48bb1405bbe2c537cfbd9e2878fd22716f2c
youngpuffy <levimuturi2@gmail.com>
2025-03-10 22:27:03 +0300

    writing code

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31
+ let num2 = 2
+ let multiply = num1 * num2
+
+ let random = Math.floor(Math.random() * 10) + 1;
+
+ let num3 = 14
+ let num4 = 5
+ let mod = num3 % num4;
+
+
+ let max =Math.max(12, 14, 18, 20, 5, 6);
